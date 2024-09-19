871470aa19939015be6ab04b5a762d0f1f162d05
Subhro-ai <subhrajyoti.singha30@gmail.com>
2024-09-20 02:37:36 +0530

    a

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,8 +13,8 @@ const app = express();
 
 app.use(cors({
     credentials:true,
-    origin:["https://cuvette-clone.onrender.com",  // Production URL
-        "https://66ec8d1f322d5d60ba131361--cuvette-clone-subhro.netlify.app/" ]
+    origin:[" http://localhost:4200",  // Local development URL
+        "https://66ec8d1f322d5d60ba131361--cuvette-clone-subhro.netlify.app"  ]
 }));
 
 app.use("/api/jobs", jobRouter)
