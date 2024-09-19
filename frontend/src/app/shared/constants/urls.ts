6886fc65f0b718009c49508eb7029157c0241eed
Subhro-ai <subhrajyoti.singha30@gmail.com>
2024-09-20 02:30:12 +0530

    a

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:4200/';
+const BASE_URL = 'https://66ec8d1f322d5d60ba131361--cuvette-clone-subhro.netlify.app/';
 
 export const JOBS_URL = BASE_URL + '/api/jobs';
 export const JOBS_FILTER = JOBS_URL + '/filter/';
