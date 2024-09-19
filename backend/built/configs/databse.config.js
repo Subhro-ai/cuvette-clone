89d6d8f9e9e9c0cdfa4a919cb21622b1aa333e98
Subhro-ai <subhrajyoti.singha30@gmail.com>
2024-09-20 00:43:56 +0530

    fixes

--- backend/built/configs/databse.config.js
+++ backend/built/configs/databse.config.js
@@ -8,6 +8,13 @@ var mongoose_1 = require("mongoose");
 var dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 var dbConnect = function () {
-    (0, mongoose_1.connect)(process.env.MONGO_URI, {}).then(function () { return console.log("Connection succesful"); }, function (error) { return console.log(error); });
+    var mongoUri = process.env.MONGO_URI;
+    if (!mongoUri) {
+        console.error("MONGO_URI is not defined in the .env file.");
+        return;
+    }
+    (0, mongoose_1.connect)(mongoUri, {})
+        .then(function () { return console.log("Connection successful"); })
+        .catch(function (error) { return console.error("MongoDB connection error:", error); });
 };
 exports.dbConnect = dbConnect;
--- backend/src/configs/databse.config.ts
+++ backend/src/configs/databse.config.ts
@@ -1,11 +1,17 @@
 import { connect, ConnectOptions } from "mongoose";
 import dotenv from 'dotenv';
-dotenv.config();
-export const dbConnect = () => {
-    connect(process.env.MONGO_URI!, {
 
-    } as ConnectOptions).then (
-        () => console.log("Connection succesful"),
-        (error) => console.log(error)
-    )
-}
+dotenv.config();
+
+export const dbConnect = () => {
+    const mongoUri = process.env.MONGO_URI;
+
+    if (!mongoUri) {
+        console.error("MONGO_URI is not defined in the .env file.");
+        return;
+    }
+
+    connect(mongoUri, {} as ConnectOptions)
+        .then(() => console.log("Connection successful"))
+        .catch((error) => console.error("MongoDB connection error:", error));
+};
